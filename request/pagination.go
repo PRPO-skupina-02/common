a13f38768181e6cd122eed572104f0f4cb336f2b
dg50914 <dg50914@student.uni-lj.si>
2025-12-20 19:59:03 +0100

    Sort support

--- request/pagination.go
+++ request/pagination.go
@@ -2,9 +2,11 @@ package request
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type PaginatedResponse struct {
@@ -49,4 +51,39 @@ func RenderPaginatedResponse(c *gin.Context, data any, total int) {
 	}
 
 	c.JSON(http.StatusOK, resp)
+}
+
+type SortOptions struct {
+	Column string
+	Desc   bool
+}
+
+func SortScope(sort *SortOptions) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Order(clause.OrderByColumn{
+			Column: clause.Column{
+				Name: sort.Column,
+			},
+			Desc: sort.Desc,
+		})
+	}
+}
+
+func GetSortOptions(c *gin.Context) *SortOptions {
+	sort := c.Query("sort")
+
+	if sort == "" {
+		return nil
+	}
+
+	desc := false
+
+	if strings.HasPrefix(sort, "-") {
+		desc = true
+	}
+
+	return &SortOptions{
+		Column: strings.ToLower(strings.TrimPrefix(sort, "-")),
+		Desc:   desc,
+	}
 }
