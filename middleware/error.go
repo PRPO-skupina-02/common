e6dc63213e6d0fb9ba61f32778bafb424dcb6d2c
David Grošelj <160850993+dg50914@users.noreply.github.com>
2026-01-10 23:58:05 +0100

    Middleware error updates (#9)

--- middleware/error.go
+++ middleware/error.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	ut "github.com/go-playground/universal-translator"
@@ -17,23 +18,37 @@ func (e *HttpError) Error() string {
 	return e.Message
 }
 
-func NewValidationError(verr validator.ValidationErrors, trans ut.Translator) HttpError {
+func NewValidationError(verr validator.ValidationErrors, trans ut.Translator) *HttpError {
 	fields := make(map[string]string)
 
 	for _, field := range verr {
 		fields[field.Field()] = field.Translate(trans)
 	}
 
-	return HttpError{
+	return &HttpError{
 		Code:    http.StatusBadRequest,
 		Message: "validation error",
 		Fields:  fields,
 	}
 }
 
-func NewNotFoundError() HttpError {
-	return HttpError{
+func NewNotFoundError() *HttpError {
+	return &HttpError{
 		Code:    http.StatusNotFound,
 		Message: "Not found",
+	}
+}
+
+func NewNamedNotFoundError(name string) *HttpError {
+	return &HttpError{
+		Code:    http.StatusNotFound,
+		Message: fmt.Sprintf("%s not found", name),
+	}
+}
+
+func NewBadRequestError(msg string) *HttpError {
+	return &HttpError{
+		Code:    http.StatusBadRequest,
+		Message: msg,
 	}
 }
